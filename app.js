c4b918386bf691bc67c389d4b8b28b0230d7f9de
Saroj Kumar <sarojk304@gmail.com>
2023-08-15 16:34:42 +0530

    bcrypt hashing enabled

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 import "dotenv/config";
 import express from "express";
 import mongoose from "mongoose";
-import encrypt from "mongoose-encryption";
+import session from "express-session";
+import passport from "passport";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const app = express();
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.use(session(
+    {
+        secret: "Our little secret.",
+        resave: false,
+        saveUninitialized: false
+    }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 // console.log(process.env.SECRET);
 
 const userSchema = new mongoose.Schema(
@@ -21,10 +35,14 @@ const userSchema = new mongoose.Schema(
 
 );
 
-const secretString = process.env.SECRET;
-userSchema.plugin(encrypt, {secret: secretString, encryptedFields: ["password"]});
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -46,13 +64,16 @@ app.post("/login", async (req, res) => {
     
     try {
         const result = await User.findOne({email: req.body.username});
-        console.log(result);
+        
         if (result!=null) {
-            if (result.password==req.body.password) {
+            const match = await bcrypt.compare(req.body.password, result.password);
+            console.log(match);
+            if (match) {
                 res.render("secrets");
             } else {
-                res.send("Password doesn't match");
+                res.send("Invaid Password");
             }
+               
         } else {
             res.send("Username doesn't exist, please registe!");
         }
@@ -64,26 +85,25 @@ app.post("/login", async (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-    
-    const newUser = new User(
-        {
-            email: req.body.username,
-            password: req.body.password
-        }
-    );
 
     try {
+        const hash = await bcrypt.hash(req.body.password, saltRounds);
+        console.log(hash);
+        const newUser = new User(
+            {
+                email: req.body.username,
+                password: hash
+            }
+        );
+
         const result = await newUser.save();
         console.log(result);
         res.render("secrets");
     } catch (error) {
         res.send(error);
     }
-
     
-
-    
-})
+});
 
 app.listen(3000, () => {
     console.log("Server started at port 3000");
